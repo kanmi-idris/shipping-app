aebd791cb9588255058c8539c25badd5475bc75b
OlaIdris <olasunkanmiidris15@gmail.com>
2024-05-12 20:16:16 +0100

    create db and table on app entry

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,17 @@
+from flask import Flask
 from api import app
+import setup
+
+
+def create_app():
+    app = Flask(__name__)
+
+    setup.create_database()
+    setup.create_tables()
+
+    return app
+
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
